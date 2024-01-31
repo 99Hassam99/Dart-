4db0a53d75fd8191a3b85624d736a79414257f62
Hassam Ahmad <144841227+99Hassam99@users.noreply.github.com>
2024-01-31 10:28:21 +0500

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,14 @@
+void main() {
+  int a = 10;
+  int b = 20;
+  int result = a + b;
+  result = a - b;
+  double divide = a / b;
+  int multiply = a * b;
+
+  print(a);
+  print(b);
+  print(result);
+  print(divide);
+  print(multiply);
+}
